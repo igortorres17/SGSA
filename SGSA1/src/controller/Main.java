102ce83c3191b5eb193198fb8d6332e180763220
herculesdev <herculesmoreira22@gmail.com>
2019-04-06 16:05:32 -0300

    Teste de formulário inicial + assinatura de commit

--- SGSA1/src/controller/Main.java
+++ SGSA1/src/controller/Main.java
@@ -8,6 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Olá Mundo!");
+        
+        view.MainForm mainForm = new view.MainForm();
+        mainForm.setVisible(true);
     }
     
 }
