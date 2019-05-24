92f5a3993749790ef087d62d4802cac7d7e52596
herculesdev <herculesmoreira22@gmail.com>
2019-05-24 09:15:45 -0300

    ajustes posição de tabs das views Pecas e Servicos

---
+++ SGSA1/src/controller/ControlePecas.java
@@ -0,0 +1,107 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Hércules M.
+ */
+public class ControlePecas implements Initializable {
+
+    @FXML
+    private TabPane abas;
+    @FXML
+    private TableView<?> tabelaServicos;
+    @FXML
+    private TextField txtPesquisar;
+    @FXML
+    private Button btnEditar;
+    @FXML
+    private Button btnVisualizar;
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private TextField txtPreco;
+    @FXML
+    private Button btnCadastrar;
+    @FXML
+    private Button btnLimpar;
+    @FXML
+    private TextField txtCodigo;
+    @FXML
+    private TextField txtEditNome;
+    @FXML
+    private TextField txtEditPreco;
+    @FXML
+    private Button btnEditSalvar;
+    @FXML
+    private Button btnEditCancelar;
+    @FXML
+    private Button btnEditExcluir;
+    @FXML
+    private TextField txtEditCodigo;
+    @FXML
+    private Button btnVisVoltar;
+    @FXML
+    private Label lblNome;
+    @FXML
+    private Label lblPreco;
+    @FXML
+    private Label lblSerial;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void txtPesquisar_keypressed(KeyEvent event) {
+    }
+
+    @FXML
+    private void btnEditar_pressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnVisualizar_pressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnCadastrar_pressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnLimpar_pressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnEditSalvar_pressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnEditCancelar_pressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnEditExcluir_pressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnVisVoltar_pressed(ActionEvent event) {
+    }
+    
+}
