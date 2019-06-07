1a31c09c0e1d6bc8d132913553708dd1dc2bcbd6
herculesdev <herculesmoreira22@gmail.com>
2019-06-07 09:56:16 -0300

    Loading assíncrono e animação de carregamento

--- SGSA1/src/controller/ControleSplash.java
+++ SGSA1/src/controller/ControleSplash.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
@@ -19,22 +20,20 @@ import model.dao.Conexao;
 public class ControleSplash extends ControleBase{
     @FXML
     private AnchorPane root;
+    @FXML
+    private ProgressIndicator progresso;
     
     @Override
     public void windowShow(WindowEvent event){
         Task task = new Task<Void>(){
-            public Void call(){
-                try {
-                    Thread.sleep(150);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-                
+            public Void call(){                    
+                conectar();
                 Platform.runLater(
-                () -> {
-                    conectar();
-                });
-                
+                    () -> {
+                        abrirJanela("/view/Login.fxml", StageStyle.TRANSPARENT);
+                        getStage().close();
+                    }
+                );
                 return null;
             }
         };
@@ -48,12 +47,10 @@ public class ControleSplash extends ControleBase{
     private void conectar(){
         try {
             Conexao.get();
-            abrirJanela("/view/Login.fxml", StageStyle.TRANSPARENT);
-            getStage().close();
         } catch (SQLException ex) {
             new Alert(AlertType.ERROR, "O Sistema falhou em conectar-se. Tente novamente mais tarde!", ButtonType.OK).showAndWait();
-            Runtime.getRuntime().exit(1);
             System.out.println("Falha ao conetar-se: " + ex.getMessage());
+            Runtime.getRuntime().exit(1);            
         }
     }
     
